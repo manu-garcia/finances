98e9f7796aa732feb3153ded5cb22571f6e764f7
Manu <garcia.martin.manu@gmail.com>
2016-11-20 01:32:17 +0000

    Working electron boiler plate

---
+++ app/main.js
@@ -0,0 +1,16 @@
+var {app, BrowserWindow} = require('electron');
+var mainWindow = null;
+
+app.on('window-all-closed', function () {
+    if(process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({width: 1200, height: 900});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+});
