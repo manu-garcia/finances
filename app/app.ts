b9fdd102d8ce56ff843c1b061104896201004b33
Manu <garcia.martin.manu@gmail.com>
2016-11-27 19:39:29 +0000

    Angular Router

--- app/app.ts
+++ app/app.ts
@@ -3,18 +3,28 @@ import './polyfills.ts';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'; 
+
 import 'font-awesome-sass-loader';
+
 import { AppComponent } from './component/app.component';
+import { AccountComponent } from './component/account.component';
+import { HomeComponent } from './component/home.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        AccountComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot([
+            { path: 'app', component: HomeComponent },
+            { path: 'account', component: AccountComponent }
+        ])
     ],
     providers: [],
     bootstrap: [AppComponent]
