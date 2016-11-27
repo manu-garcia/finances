c1990b2a76f658db58f88c9203716779938d9eb6
Manu <garcia.martin.manu@gmail.com>
2016-11-27 19:38:39 +0000

    Angular Router added Account component added

---
+++ app/component/account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account-component',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.scss']    
+})
+export class AccountComponent {
+
+    constructor () {
+
+    }
+}
---
+++ app/component/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home-component',
+    templateUrl: './home.template.html',
+    styleUrls: []
+})
+export class HomeComponent {
+
+    constructor () {
+
+    }
+}
