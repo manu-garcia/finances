efb10877d33ddf103e40122ff988547f4d580bdc
Manu <garcia.martin.manu@gmail.com>
2016-11-30 22:07:54 +0000

    fontawesome working in dev and packed app

--- webpack.config.js
+++ webpack.config.js
@@ -47,11 +47,11 @@ module.exports = {
             },
             { 
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=../../../../../resources/fonts/[name].[ext]"
+                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[name].[ext]&publicPath=dist/"
             },
             {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader" 
+                loader: "file-loader?name=[name].[ext]" 
             }
         ]
     },
